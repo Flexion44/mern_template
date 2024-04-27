8f5fc28ebe680045228649f72865158ef661cf15
Alban Le Bail <albanlebail56@gmail.com>
2024-04-27 20:23:28 +0200

    correction of the mongo URL

--- api/config/index.js
+++ api/config/index.js
@@ -4,7 +4,7 @@ export default {
     port : process.env.PORT || 5050,
     mongo : {
         fullPath : process.env.MONGO_FULL_PATH || undefined,
-        host : process.env.MONGO_HOST || 'localhost',
+        host : process.env.MONGO_HOST || 'mongo-db',
         port : process.env.MONGO_PORT || 27017,
         db : process.env.MONGO_DB || 'database_name',
         user : process.env.MONGO_USER || '',
